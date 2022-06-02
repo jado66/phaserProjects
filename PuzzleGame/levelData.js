dc6b506b4587133a25bd71f3ed4092b4d91d3a04
jado66 <jado66@gmail.com>
2022-06-01 22:56:32 -0400

    init

---
+++ PuzzleGame/levelData.js
@@ -0,0 +1,128 @@
+levelData = {
+    1:{
+        goal:[390,223.3],
+        blocky:[100, 450],
+        blocks:[
+            ['s', 600, 450],
+            ['s', 25, 450],
+            ['s', 100, 550],
+            ['s', 500,523.3],
+            ['s', 473.3,35],
+            ['s', 35,62.3],
+            ['s', 570,62.3],
+            ['s', 543.3,250]
+        ]
+    },
+    2:{
+        levelMap: " S       S          \n" + 
+                  "S            S      \n" + 
+                  "   S              S \n" + 
+                  "               S    \n" + 
+                  "        G           \n" + 
+                  "           S        \n" + 
+                  "  S                S\n" + 
+                  "      S             \n" + 
+                  "S                   \n" + 
+                  "S    S     P  S  S  \n" + 
+                  "       S            \n" + 
+                  "                    \n" + 
+                  "  S                S\n" + 
+                  "S           SS      \n" + 
+                  " S          S     S \n"                 
+    },
+    "Editor":{
+        goal:[390,240],
+        blocky:[200, 20],
+        levelMap:"                    \n"+
+                 "                    \n"+
+                 "                    \n"+
+                 "                    \n"+
+                 "        G           \n"+
+                 "                    \n"+
+                 "                    \n"+
+                 "                    \n"+
+                 "                    \n"+
+                 "         P          \n"+
+                 "                    \n"+
+                 "                    \n"+
+                 "                    \n"+
+                 "                    \n"+
+                 "                    \n",
+        blocks:[
+            ['s', 20, 20],
+            ['m', 60, 60],
+            ['s', 100, 100],
+            ['s', 140, 140]
+        ]
+            
+    },
+    3:{
+        levelMap: " S       S          \n" + 
+                  "S            S      \n" + 
+                  "   S              S \n" + 
+                  "               S    \n" + 
+                  "        GS          \n" + 
+                  "                    \n" + 
+                  "  S                S\n" + 
+                  "      S             \n" + 
+                  "S                   \n" + 
+                  "S    S     P  M  S  \n" + 
+                  "       S            \n" + 
+                  "                    \n" + 
+                  "  S                S\n" + 
+                  "S          SSS      \n" + 
+                  " S          S S   S \n"
+    },
+    
+    4:{
+        levelMap: "  S S S S S S SSSSSS\n" + 
+                  "   S        S S  SGS\n" + 
+                  "S    S        S  S S\n" + 
+                  "SS        S      S S\n" + 
+                  "S       SS     S S S\n" + 
+                  "S S     SS    SS S S\n" + 
+                  "S   S   S    S    PS\n" + 
+                  "SS      S    S    SS\n" + 
+                  "                   S\n" + 
+                  "S      SS        S S\n" + 
+                  "S  S         S   S S\n" + 
+                  "S     S      S SS  S\n" + 
+                  "SS       SS        S\n" + 
+                  "S    S             S\n" + 
+                  "SSSSSSS SSS SSSSSSSS\n"
+    },
+    5:{
+        levelMap: "SS S    SSSSSSSSSSSS\n" + 
+                  "S                  S\n" + 
+                  "                    \n" + 
+                  "                    \n" + 
+                  "                    \n" + 
+                  "S  P   M   M        \n" + 
+                  "S     M     M       \n" + 
+                  "S    M  G    M      \n" + 
+                  "S   M         M     \n" + 
+                  "S  M           M    \n" + 
+                  "S               M   \n" + 
+                  "S                M  \n" + 
+                  "S                 M \n" + 
+                  "                   S\n" + 
+                  " SSSSSSS          S \n"
+    },
+    5:{
+        levelMap: "P                   \n" + 
+                  "                    \n" + 
+                  "                    \n" +
+                  "                    \n" +
+                  "                    \n" +
+                  "                    \n" + 
+                  "                    \n" +
+                  "                    \n" +
+                  "                    \n" +
+                  "                    \n" +
+                  "                    \n" +
+                  "                    \n" + 
+                  "                    \n" +
+                  "                    \n" +
+                  "                   G\n" 
+    },
+}
